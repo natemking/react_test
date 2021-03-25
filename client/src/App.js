9dd1c575f49f35aa4e5cedb99b59ed1b7b24f384
Nate M King <72567521+natemking@users.noreply.github.com>
2021-03-25 14:33:53 -0400

    Imported stled-components and set up GlobalStyles.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import GlobalStyles from './GlobalStyles';
+import { H1 } from './elements';
+
+
+// const theme = {
+//   fontFamily: `'Montserrat', sans- serif`,
+// }
 
 function App() {
   return (
     <>
+      <GlobalStyles />
+      <H1>Hello World</H1>
     </>
+    
   );
 }
 
